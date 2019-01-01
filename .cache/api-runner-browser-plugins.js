5f6bb03380043c0cd40ef53473ed27379c0534ae
Jefferson Oliveira <cardbeatt@gmail.com>
2019-01-01 21:47:19 -0200

    changed boilerplate

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('/home/jefferson/Documents/Projects/wallnephel-ws/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,24 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-
-const outputDirectory = 'dist';
 
 module.exports = {
-  entry: ['babel-polyfill', './src/client/index.js'],
+  entry: './src/index.js',
   output: {
-    path: path.join(__dirname, outputDirectory),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            "presets" : ["env", "react"]
+          }
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000'
       }
     ]
-  },
-  devServer: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': 'http://localhost:8080'
-    }
-  },
-  plugins: [
-    new CleanWebpackPlugin([outputDirectory]),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      favicon: './public/favicon.ico'
-    })
-  ]
+  }
 };
