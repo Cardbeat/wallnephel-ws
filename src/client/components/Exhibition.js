fd0cf3a324cd6f0ad6f4dfac06b8aeb3611e9ad3
Jefferson Oliveira <cardbeatt@gmail.com>
2019-01-10 20:24:26 -0200

    added exhibition

---
+++ src/client/components/Exhibition.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class Exhibition extends Component {
+    render() {
+        return (
+            <div>
+                <h1> esta sao as obras primas da sarah</h1>
+            </div>
+        )
+    }
+}
