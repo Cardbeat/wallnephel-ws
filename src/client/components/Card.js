c308ddad63d698f2bbb961a7742b8cc13753df08
Jefferson Oliveira <cardbeatt@gmail.com>
2019-01-11 08:23:29 -0200

    added materialize cards and bulma css

--- src/client/components/Card.js
+++ src/client/components/Card.js
@@ -21,8 +21,18 @@ export default class Exhibition extends Component {
 
     render() {
         return (
-            <div>
-                <img src={this.state.url} height='600' width='400' />
+            <div className="card small">
+                <div className="card-image waves-effect waves-block waves-light">
+                    <img className="activator" src={this.state.url} />>
+                </div>
+                <div className="card-content">
+                    <span className="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+                    <p><a href="#">This is a link</a></p>
+                </div>
+                <div className="card-reveal">
+                    <span className="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                    <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                </div>
             </div>
         )
     }
