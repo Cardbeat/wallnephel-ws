4b8a26c1ec9aeddc15bab4795ca67352bb6045e4
Jefferson Oliveira <cardbeatt@gmail.com>
2019-01-15 17:25:55 -0200

    design login

--- src/client/components/Login.js
+++ src/client/components/Login.js
@@ -44,19 +44,29 @@ export default class Login extends Component {
         }} />
       }
       return (
-        <div>
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                Name:
-                <input type="text" required id="name" />
-                </label>
-                <label>
-                Password:
-                <input type="password" required id="password" />
-                </label>
-                <input type="submit" value="Submit" />
-                </form>
+        <div className='background'>
+        <div className="login">
+                <div className="row">
+                  <div className="col s12">
+                    <div className="card white">
+                      <div className="card-content">
+                        <form onSubmit={this.handleSubmit}>
+                          <label>
+                            Name:
+                            <input type="text" required id="name" />
+                          </label>
+                          <label>
+                            Password:
+                            <input type="password" required id="password" />
+                          </label>
+                        <input className='btn waves-light blue darken-4 loginButton' type="submit" value="Login" />
+                      </form>
+                  </div>
+                </div>
+              </div>
+          </div>
         </div>
+       </div>
       );
     }
   }
